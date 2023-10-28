51c9b6055eb61c417bbca4ccf7051e5427a8f7d1
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-10-28 08:51:35 +0200

    added customer model and basic views with controller

---
+++ LibApp/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace LibApp.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Customer()
+        {
+
+        }
+    }
+}
---
+++ LibApp/Views/Customers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibApp.Views.Customers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
