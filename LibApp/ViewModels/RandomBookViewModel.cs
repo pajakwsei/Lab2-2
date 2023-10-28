a609a5504bb4b725c6664ee41db8d3e58a010a7e
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-10-28 08:45:19 +0200

    Added view model

---
+++ LibApp/ViewModels/RandomBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LibApp.Models;
+
+namespace LibApp.ViewModels
+{
+    public class RandomBookViewModel
+    {
+        public Book Book { get; set; }
+        public List<Customer> Customers { get; set; }
+        public RandomBookViewModel()
+        {
+        
+        }
+    }
+}
