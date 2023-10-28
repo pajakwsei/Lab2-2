6cf6b95dae7f4ff4ec05dbf3bbd8d49aab0742f7
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-10-28 09:18:39 +0200

    Added Data Annotations to Customer model

--- LibApp/Models/Customer.cs
+++ LibApp/Models/Customer.cs
@@ -1,8 +1,13 @@
-﻿namespace LibApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        // Data Annotation - convention name
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
         public MembershipType MembershipType { get; set; }
         public Customer()
