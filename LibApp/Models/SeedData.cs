78a7ac737dbe2303aed03ca732a9f687c3af8045
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-10-28 09:10:57 +0200

    Added class for data seeding

---
+++ LibApp/Models/SeedData.cs
@@ -0,0 +1,33 @@
+﻿using LibApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context =
+                new ApplicationDbContext(
+                    serviceProvider.GetRequiredService<
+                        DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any MembershipTypes
+                if (context.MembershipTypes.Any())
+                {
+                    Console.WriteLine("Database already seeded");
+                    return;
+                }
+
+                context.MembershipTypes.AddRange(
+                    new MembershipType { Id = 1, SignUpFee = 0, DurationInMonths = 0, DiscountRate = 0 },
+                    new MembershipType { Id = 2, SignUpFee = 30, DurationInMonths = 1, DiscountRate = 5 },
+                    new MembershipType { Id = 3, SignUpFee = 90, DurationInMonths = 3, DiscountRate = 10 },
+                    new MembershipType { Id = 4, SignUpFee = 300, DurationInMonths = 12, DiscountRate = 20 }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
