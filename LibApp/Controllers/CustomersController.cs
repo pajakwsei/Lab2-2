e6e332dc881cd4d2e6477db7ed0e62ec8307d772
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-11-25 08:37:56 +0100

    Added membership type names

--- LibApp/Controllers/CustomersController.cs
+++ LibApp/Controllers/CustomersController.cs
@@ -27,7 +27,9 @@ namespace LibApp.Controllers
 
         public IActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers
+                .Include(c => c.MembershipType)
+                .SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
