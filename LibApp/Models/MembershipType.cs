0f63522f82803598cccb0c8b21e94e6891888c5d
pajakwsei <92627074+pajakwsei@users.noreply.github.com>
2023-11-25 08:29:14 +0100

    Updated MembershipTypes names with migration SQL

--- LibApp/Models/MembershipType.cs
+++ LibApp/Models/MembershipType.cs
@@ -1,8 +1,12 @@
-﻿namespace LibApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Models
 {
     public class MembershipType
     {
         public byte Id { get; set; }
+        [Required]
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
